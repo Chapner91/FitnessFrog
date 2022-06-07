872dfc1e9ed16b67148f4c7a548af87cdd58aa43
Jason Hapner <chapner@gmail.com>
2022-06-07 10:38:55 -0400

    updated the entries controller to add invalid form field values

--- Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -9,59 +9,68 @@ using Treehouse.FitnessFrog.Models;
 
 namespace Treehouse.FitnessFrog.Controllers
 {
-    public class EntriesController : Controller
-    {
-        private EntriesRepository _entriesRepository = null;
+	public class EntriesController : Controller
+	{
+		private EntriesRepository _entriesRepository = null;
 
-        public EntriesController()
-        {
-            _entriesRepository = new EntriesRepository();
-        }
+		public EntriesController()
+		{
+			_entriesRepository = new EntriesRepository();
+		}
 
-        public ActionResult Index()
-        {
-            List<Entry> entries = _entriesRepository.GetEntries();
+		public ActionResult Index()
+		{
+			List<Entry> entries = _entriesRepository.GetEntries();
 
-            // Calculate the total activity.
-            double totalActivity = entries
-                .Where(e => e.Exclude == false)
-                .Sum(e => e.Duration);
+			// Calculate the total activity.
+			double totalActivity = entries
+				.Where(e => e.Exclude == false)
+				.Sum(e => e.Duration);
 
-            // Determine the number of days that have entries.
-            int numberOfActiveDays = entries
-                .Select(e => e.Date)
-                .Distinct()
-                .Count();
+			// Determine the number of days that have entries.
+			int numberOfActiveDays = entries
+				.Select(e => e.Date)
+				.Distinct()
+				.Count();
 
-            ViewBag.TotalActivity = totalActivity;
-            ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
+			ViewBag.TotalActivity = totalActivity;
+			ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
 
-            return View(entries);
-        }
+			return View(entries);
+		}
 
-        public ActionResult Add()
-        {
-            return View();
-        }
+		public ActionResult Add()
+		{
+			return View();
+		}
 
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+		[HttpPost]
+		public ActionResult Add(DateTime? date, 
+			int? activityId, double? duration,
+			Entry.IntensityLevel? intensity,
+			bool exclude, string notes)
+		{		
+			return View();
+		}
 
-            return View();
-        }
+		public ActionResult Edit(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
 
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+			return View();
+		}
 
-            return View();
-        }
-    }
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+
+			return View();
+		}
+	}
 }
